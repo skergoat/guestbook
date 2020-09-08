35759b235e13dc60517bdb5081d8f2220b70947c
skergoatweb@gmail.com <skergoatweb@gmail.com>
2020-09-09 04:27:33 +0700

    security

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -96,7 +96,7 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator implements Passwor
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('easyadmin'));
+        return new RedirectResponse($this->urlGenerator->generate('admin'));
     }
 
     protected function getLoginUrl()
